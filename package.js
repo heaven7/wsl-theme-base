a6891b69096a1bd0e22bf1073faa2145be0f7de1
Christian Grlica <christian@heavenseven.net>
2015-07-27 02:57:44 +0200

    cleanup

--- package.js
+++ package.js
@@ -11,38 +11,29 @@ both = ['client','server'];
 Package.onUse(function(api) {
     api.versionsFrom('1.1.0.2');
 
-    api.addFiles([
-        'package-tap.i18n'
-    ], both);
+    api.addFiles('package-tap.i18n', both);
 
-    api.use(
-        [
-            'heaven7:wsl-core',
-            'heaven7:wsl-i18n'
-        ], both);
+    api.use([
+        'heaven7:wsl-core',
+        'heaven7:wsl-i18n'
+    ], both);
 
     api.imply('heaven7:wsl-i18n');
 
-    api.addFiles(
-        [
-            'lib/client/layouts/main.html',
-            'lib/client/templates/dashboard.html'
-        ],
-        'client');
+    api.addFiles([
+        'lib/client/layouts/main.html',
+        'lib/client/templates/dashboard.html'
+    ], 'client');
 
-    api.addFiles(
-        [
-            'lib/both/config.js',
-            'lib/both/routes.js',
-            'lib/both/theme-base.js'
-        ],
-        both);
+    api.addFiles([
+        'lib/both/config.js',
+        'lib/both/routes.js',
+        'lib/both/theme-base.js'
+    ], both);
 
-    api.addFiles(
-        [
-            'i18n/de.i18n.json',
-            'i18n/en.i18n.json',
-            'i18n/es.i18n.json'
-        ],
-        both);
+    api.addFiles([
+        'i18n/de.i18n.json',
+        'i18n/en.i18n.json',
+        'i18n/es.i18n.json'
+    ], both);
 });
