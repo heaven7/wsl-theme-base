0f750cd7779739c610beac2e58c3efce97375e7b
Christian Grlica <christian@heavenseven.net>
2015-11-12 15:59:16 +0100

    semantic ui setup

--- package.js
+++ package.js
@@ -15,7 +15,11 @@ Package.onUse(function(api) {
         'heaven7:wsl-core@0.0.2',
         'heaven7:wsl-i18n@0.0.2',
         'zimme:iron-router-auth@3.2.0-rc.4',
-        'iron:router@1.0.12'
+        'iron:router@1.0.12',
+        'semantic:ui@1.12.1_1',
+        'semantic:ui-css@1.9.3',
+        'fabienb4:autoform-semantic-ui@0.1.3',
+        'flemay:less-autoprefixer@1.0.2'
     ];
 
     api.use(packages, both);
@@ -23,6 +27,8 @@ Package.onUse(function(api) {
     api.imply(packages);
 
     api.addFiles([
+        'lib/client/startup.js',
+        'lib/client/semantic-ui/custom.semantic.json',
         'lib/client/layouts/main.html',
         'lib/client/layouts/main.js',
         'lib/client/templates/dashboard.html',
